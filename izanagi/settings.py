3445039c17fe1b6997e237dc4100188dc480b4b1
Ryuk-Togo <ryuk.togo.1026@gmail.com>
2018-11-30 00:04:44 +0900

    initialize susanowo

--- izanagi/settings.py
+++ izanagi/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    # 'susanowo.apps.SusanowoConfig',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
