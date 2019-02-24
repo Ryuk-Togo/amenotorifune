c9841c3fa707b5a004c57fde285a13f8227b1484
Ryuk-Togo <ryuk.togo.1026@gmail.com>
2019-02-24 23:13:12 +0900

    add todo

---
+++ sql/T_TODO.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IZANAMI.T_TODO
+  ( TODO_ID             int
+  , TITLE               varchar(30)
+  , DISCRIPTION         varchar(200)
+  , SHOULD_ACTION       int
+  , WHERE_DONT_ACTION   int
+  , SINGLE_ACTON        int
+  , CAN_DO_TOW_MINION   int
+  , SHOULD_MYSELF       int
+  , SHOULD_DO_THAN_2MIN int
+  , USER_ID             varchar(6)
+  , CATEGORY            varchar(2)
+  , CREATE_DATE         date
+  , CREATE_PG_ID        varchar(30)
+  , CREATE_USER_ID      varchar(6)
+  , UPDATE_DATE         date
+  , UPDATE_PG_ID        varchar(30)
+  , UPDATE_USER_ID      varchar(6)
+  )
