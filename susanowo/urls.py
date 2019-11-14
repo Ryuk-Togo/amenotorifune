c1fff730b5b30d0db0bf297de965e53bb9c615b0
Ryuk-Togo <ryuk.togo.1026@gmail.com>
2019-11-14 15:00:31 +0900

    完了・削除は色を同的に変える

--- susanowo/urls.py
+++ susanowo/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^index/$', views.indexviews.index, name='index'),
     url(r'^inbox/$', views.inboxviews.inbox, name='inbox'),
     url(r'^inboxupd/$', views.inboxviews.inboxupd, name='inboxupd'),
+    url(r'^modstatus/$', views.indexviews.modstatus, name='modstatus'),
 ]
