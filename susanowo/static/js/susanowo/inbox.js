82eef99b6d2c7253e5ff4d81454192c20642385a
Ryuk-Togo <ryuk.togo.1026@gmail.com>
2019-11-21 15:58:30 +0900

    項目制御の方法をjs関数化

--- susanowo/static/js/susanowo/inbox.js
+++ susanowo/static/js/susanowo/inbox.js
@@ -1,6 +1,81 @@
 $(document).ready(function(){
-    $('input[name="user_id"]').prop('disabled', true);
-//    $('input[name="action_selection"]').prop('disabled', true);
+//     $('input[name="user_id"]').prop('disabled', true);
+// //    $('input[name="action_selection"]').prop('disabled', true);
+//     $('#id_action_selection').prop('disabled', false);
+//     $('input[name="delivery_date"]').prop('disabled', true);
+//     $('input[name="single_action"]').prop('disabled', true);
+//     $('input[name="can_do_tow_minite"]').prop('disabled', true);
+//     $('input[name="should_myself"]').prop('disabled', true);
+//     $('input[name="request_pertner"]').prop('disabled', true);
+//     $('input[name="should_do_oneday"]').prop('disabled', true);
+    controlObuject();
+});
+$(function() {
+    // 行動を起こす必要がある
+    $('input[name="should_action"]').change(function() {
+        // var is_should_action = $('input[name="should_action"]').prop('checked');
+        // if (is_should_action) {
+        //     // 行動を起こす必要がある
+        //     $('#id_action_selection').prop('disabled', true);
+        //     $('input[name="delivery_date"]').prop('disabled', false);
+        //     $('input[name="single_action"]').prop('disabled', false);
+        // } else {
+        //     // 行動を起こす必要はない
+        //     $('#id_action_selection').prop('disabled', false);
+        //     $('input[name="delivery_date"]').prop('disabled', true);
+        //     $('input[name="single_action"]').prop('disabled', true);
+        //     $('input[name="can_do_tow_minite"]').prop('disabled', true);
+        //     $('input[name="should_myself"]').prop('disabled', true);
+        //     $('input[name="request_pertner"]').prop('disabled', true);
+        //     $('input[name="should_do_oneday"]').prop('disabled', true);
+        //         }
+        controlObuject();
+    });
+
+    // アクションは１つ
+    $('input[name="single_action"]').change(function() {
+        // var is_single_action = $('input[name="single_action"]').prop('checked');
+        // if (is_single_action) {
+        //     $('input[name="can_do_tow_minite"]').prop('disabled', false);
+        // } else {
+        //     $('input[name="can_do_tow_minite"]').prop('disabled', true);
+        //     $('input[name="should_myself"]').prop('disabled', true);
+        //     $('input[name="request_pertner"]').prop('disabled', true);
+        //     $('input[name="should_do_oneday"]').prop('disabled', true);
+        //         }
+        controlObuject();
+    });
+
+    // 2分以内でおわる？
+    $('input[name="can_do_tow_minite"]').change(function() {
+        // var is_can_do_tow_minite = $('input[name="can_do_tow_minite"]').prop('checked');
+        // if (is_can_do_tow_minite) {
+        //     $('input[name="should_myself"]').prop('disabled', false);
+        // } else {
+        //     $('input[name="should_myself"]').prop('disabled', true);
+        //     $('input[name="request_pertner"]').prop('disabled', true);
+        //     $('input[name="should_do_oneday"]').prop('disabled', true);
+        // }
+        controlObuject();
+    });
+
+    // 自分でやるべき
+    $('input[name="should_myself"]').change(function() {
+        // var is_should_myself = $('input[name="should_myself"]').prop('checked');
+        // if (is_should_myself) {
+        //     $('input[name="request_pertner"]').prop('disabled', false);
+        //     $('input[name="should_do_oneday"]').prop('disabled', false);
+        // } else {
+        //     $('input[name="request_pertner"]').prop('disabled', true);
+        //     $('input[name="should_do_oneday"]').prop('disabled', true);
+        // }
+        controlObuject();
+    });
+});
+
+function controlObuject() {
+
+    // 初期状態
     $('#id_action_selection').prop('disabled', false);
     $('input[name="delivery_date"]').prop('disabled', true);
     $('input[name="single_action"]').prop('disabled', true);
@@ -8,63 +83,56 @@ $(document).ready(function(){
     $('input[name="should_myself"]').prop('disabled', true);
     $('input[name="request_pertner"]').prop('disabled', true);
     $('input[name="should_do_oneday"]').prop('disabled', true);
-});
-$(function() {
-    // 行動を起こす必要がある
-    $('input[name="should_action"]').change(function() {
-        var is_should_action = $('input[name="should_action"]').prop('checked');
-        if (is_should_action) {
-            // 行動を起こす必要がある
-            $('#id_action_selection').prop('disabled', true);
-            $('input[name="delivery_date"]').prop('disabled', false);
-            $('input[name="single_action"]').prop('disabled', false);
-        } else {
-            // 行動を起こす必要はない
-            $('#id_action_selection').prop('disabled', false);
-            $('input[name="delivery_date"]').prop('disabled', true);
-            $('input[name="single_action"]').prop('disabled', true);
-            $('input[name="can_do_tow_minite"]').prop('disabled', true);
-            $('input[name="should_myself"]').prop('disabled', true);
-            $('input[name="request_pertner"]').prop('disabled', true);
-            $('input[name="should_do_oneday"]').prop('disabled', true);
-                }
-    });
 
-    // アクションは１つ
-    $('input[name="single_action"]').change(function() {
-        var is_single_action = $('input[name="single_action"]').prop('checked');
-        if (is_single_action) {
-            $('input[name="can_do_tow_minite"]').prop('disabled', false);
-        } else {
-            $('input[name="can_do_tow_minite"]').prop('disabled', true);
-            $('input[name="should_myself"]').prop('disabled', true);
-            $('input[name="request_pertner"]').prop('disabled', true);
-            $('input[name="should_do_oneday"]').prop('disabled', true);
-                }
-    });
+    var is_should_action = $('input[name="should_action"]').prop('checked');
+    if (is_should_action) {
+        // 行動を起こす必要がある
+        $('#id_action_selection').prop('disabled', true);
+        $('input[name="delivery_date"]').prop('disabled', false);
+        $('input[name="single_action"]').prop('disabled', false);
+    } else {
+        // 行動を起こす必要はない
+        $('#id_action_selection').prop('disabled', false);
+        $('input[name="delivery_date"]').prop('disabled', true);
+        $('input[name="single_action"]').prop('disabled', true);
+        $('input[name="can_do_tow_minite"]').prop('disabled', true);
+        $('input[name="should_myself"]').prop('disabled', true);
+        $('input[name="request_pertner"]').prop('disabled', true);
+        $('input[name="should_do_oneday"]').prop('disabled', true);
+    }
 
-    // 2分以内でおわる？
-    $('input[name="can_do_tow_minite"]').change(function() {
-        var is_can_do_tow_minite = $('input[name="can_do_tow_minite"]').prop('checked');
-        if (is_can_do_tow_minite) {
-            $('input[name="should_myself"]').prop('disabled', false);
-        } else {
-            $('input[name="should_myself"]').prop('disabled', true);
-            $('input[name="request_pertner"]').prop('disabled', true);
-            $('input[name="should_do_oneday"]').prop('disabled', true);
-        }
-    });
+    var is_single_action = $('input[name="single_action"]').prop('checked');
+    if (is_single_action) {
+        // アクションは複数
+        $('input[name="can_do_tow_minite"]').prop('disabled', false);
+    } else {
+        // アクションは１つ
+        $('input[name="can_do_tow_minite"]').prop('disabled', true);
+        $('input[name="should_myself"]').prop('disabled', true);
+        $('input[name="request_pertner"]').prop('disabled', true);
+        $('input[name="should_do_oneday"]').prop('disabled', true);
+    }
 
-    // 自分でやるべき
-    $('input[name="should_myself"]').change(function() {
-        var is_should_myself = $('input[name="should_myself"]').prop('checked');
-        if (is_should_myself) {
-            $('input[name="request_pertner"]').prop('disabled', false);
-            $('input[name="should_do_oneday"]').prop('disabled', false);
-        } else {
-            $('input[name="request_pertner"]').prop('disabled', true);
-            $('input[name="should_do_oneday"]').prop('disabled', true);
-        }
-    });
+    var is_can_do_tow_minite = $('input[name="can_do_tow_minite"]').prop('checked');
+    if (is_can_do_tow_minite) {
+        // 2分以上かかる
+        $('input[name="should_myself"]').prop('disabled', false);
+    } else {
+        // 2分以上で終わる
+        $('input[name="should_myself"]').prop('disabled', true);
+        $('input[name="request_pertner"]').prop('disabled', true);
+        $('input[name="should_do_oneday"]').prop('disabled', true);
+    }
 
-});
+    var is_should_myself = $('input[name="should_myself"]').prop('checked');
+    if (is_should_myself) {
+        // 誰かにお願いする
+        $('input[name="request_pertner"]').prop('disabled', false);
+        $('input[name="should_do_oneday"]').prop('disabled', false);
+    } else {
+        // 自分で行う
+        $('input[name="request_pertner"]').prop('disabled', true);
+        $('input[name="should_do_oneday"]').prop('disabled', true);
+    }
+
+}
